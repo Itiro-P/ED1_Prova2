f1087ba9b5bf1f3eea65ddf9e7174518865b6d80
Pedro N <itiropedro@gmail.com>
2025-05-20 15:19:16 -0300

    oq os cara faz cmg

--- codigo/busca.cpp
+++ codigo/busca.cpp
@@ -1,3 +1,41 @@
-int main() {
-    return 0;
+template <typename T>
+
+void selection_sort(T vet){
+    for(int i = 0; i < vet.size(); i++){
+        T minimun = &(vet.i);
+        for(int j = i; j < vet.size(); j++){
+            if(*minimun > vet.j){
+                *minimun = vet.j;
+            }
+        }
+        swip(*minimun, vet.i);
+    }
 }
+
+template <typename T>
+
+void bubble_sort_padrao(T vet){
+    for(int i = 0; i < vet.size(); i++){
+        for(int j = 0; j+1 < vet.size(); j++){
+            if(vet.j > vet.(j+1)){
+                swip(vet.j,  vet.(j+1));
+            }
+        }
+    }
+}
+
+template <typename T>
+
+void bubble_sort_otimizado(T vet){
+    bool verificacao = true;
+    while(verificacao){
+        verificacao = false;
+        for(int j = 0; j+1 < vet.size(); j++){
+            if(vet.j > vet.(j+1)){
+                swip(vet.j,  vet.(j+1));
+                verificacao = true;
+            }
+        }
+    }
+}
+
